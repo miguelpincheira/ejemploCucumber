ebd24c4d4d11550ed7422387eded6d52e6bcae11
miguel <miguel@172.17.100.159>
2016-03-10 17:58:04 -0300

    Limpieza de Codigo, solo Mockito

--- src/test/java/cl/nisum/ubb/RentalServiceTest.java
+++ src/test/java/cl/nisum/ubb/RentalServiceTest.java
@@ -7,14 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.internal.matchers.Any;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
